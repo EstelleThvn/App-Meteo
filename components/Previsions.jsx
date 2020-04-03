3d1e704278b47cc6bf243183033f1f24fe0dc456
Estelle Thouvenin <estelle.thouvenin00@gmail.com>
2020-04-03 12:09:45 +0200

    ajout previsions jour 4 et 5

--- components/Previsions.jsx
+++ components/Previsions.jsx
@@ -19,24 +19,46 @@ export default function Previsions() {
   const [date3, setDate3] = useState('');
   const [icon3, setIcon3] = useState('');
 
+  const [prevTemp4, setPrevTemp4] = useState('');
+  const [desc4, setDesc4] = useState('');
+  const [date4, setDate4] = useState('');
+  const [icon4, setIcon4] = useState('');
+
+  const [prevTemp5, setPrevTemp5] = useState('');
+  const [desc5, setDesc5] = useState('');
+  const [date5, setDate5] = useState('');
+  const [icon5, setIcon5] = useState('');
+
   fetch(
           `https://api.openweathermap.org/data/2.5/forecast?lat=48.853&lon=2.35&units=metric&lang=fr&appid=fcaa070d1babb38b0d7838801ad55e67`
       )
           .then((response) => response.json())
-          .then(data =>{console.log(data.list); setPrevTemp1(data.list[8].main.temp);
-          setDesc1(data.list[8].weather[0].description); 
-          setDate1(data.list[8].dt_txt);
-          setIcon1(data.list[8].weather[0].icon); 
+          .then(data =>{console.log(data.list);
+            
+          setPrevTemp1(data.list[7].main.temp);
+          setDesc1(data.list[7].weather[0].description); 
+          setDate1(data.list[7].dt_txt);
+          setIcon1(data.list[7].weather[0].icon); 
 
-          setPrevTemp2(data.list[16].main.temp);
-          setDesc2(data.list[16].weather[0].description); 
-          setDate2(data.list[16].dt_txt);
-          setIcon2(data.list[16].weather[0].icon); 
+          setPrevTemp2(data.list[15].main.temp);
+          setDesc2(data.list[15].weather[0].description); 
+          setDate2(data.list[15].dt_txt);
+          setIcon2(data.list[15].weather[0].icon); 
 
-          setPrevTemp3(data.list[24].main.temp);
-          setDesc3(data.list[24].weather[0].description); 
-          setDate3(data.list[24].dt_txt);
-          setIcon3(data.list[24].weather[0].icon); 
+          setPrevTemp3(data.list[23].main.temp);
+          setDesc3(data.list[23].weather[0].description); 
+          setDate3(data.list[23].dt_txt);
+          setIcon3(data.list[23].weather[0].icon);
+          
+          setPrevTemp4(data.list[31].main.temp);
+          setDesc4(data.list[31].weather[0].description); 
+          setDate4(data.list[31].dt_txt);
+          setIcon4(data.list[31].weather[0].icon);
+
+          setPrevTemp5(data.list[39].main.temp);
+          setDesc5(data.list[39].weather[0].description); 
+          setDate5(data.list[39].dt_txt);
+          setIcon5(data.list[39].weather[0].icon);
           });
 
           let dateCourt1 = date1.split(" ")[0];
@@ -50,6 +72,14 @@ export default function Previsions() {
           let dateCourt3 = date3.split(" ")[0];
           let jour3 = dateCourt3.split("-")[2];
           let mois3 = dateCourt3.split("-")[1];
+
+          let dateCourt4 = date4.split(" ")[0];
+          let jour4 = dateCourt4.split("-")[2];
+          let mois4 = dateCourt4.split("-")[1];
+
+          let dateCourt5 = date5.split(" ")[0];
+          let jour5 = dateCourt5.split("-")[2];
+          let mois5 = dateCourt5.split("-")[1];
   
     return (
         <View style={{backgroundColor:'#ffffff12', paddingLeft:'0.5rem', paddingRight:'0.5rem', paddingBottom: '1.5rem', borderRadius: 10}}>
@@ -73,6 +103,20 @@ export default function Previsions() {
           <Text style={styles.temperature}>{prevTemp3}°</Text>
           {icon3 !== '' && <Image style={styles.temps} source={require(`../assets/${icon3}.png`)}/>}
           <Text style={styles.description}>{desc3}</Text>
+          </View>
+
+          <View style={{flexDirection:'row', alignItems:'flex-end', justifyContent:'space-between', borderBottomWidth: 2, borderBottomColor: '#FEFEFE5f', paddingBottom:'1.5rem', paddingTop:'2.5rem'}}>
+          <Text style={styles.date}>{jour4}/{mois4}</Text>
+          <Text style={styles.temperature}>{prevTemp4}°</Text>
+          {icon4 !== '' && <Image style={styles.temps} source={require(`../assets/${icon4}.png`)}/>}
+          <Text style={styles.description}>{desc4}</Text>
+          </View>
+
+          <View style={{flexDirection:'row', alignItems:'flex-end', justifyContent:'space-between', borderBottomWidth: 2, borderBottomColor: '#FEFEFE5f', paddingBottom:'1.5rem', paddingTop:'2.5rem'}}>
+          <Text style={styles.date}>{jour5}/{mois5}</Text>
+          <Text style={styles.temperature}>{prevTemp5}°</Text>
+          {icon5 !== '' && <Image style={styles.temps} source={require(`../assets/${icon5}.png`)}/>}
+          <Text style={styles.description}>{desc5}</Text>
           </View>
 
         </View>
